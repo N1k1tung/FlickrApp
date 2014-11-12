733c624e4dca7d901f5b9fa412cc03087ac79f69
Nikita Rodin <nrodin@nrodin-iMac.local>
2014-11-12 11:33:47 +0300

    Initial Commit

---
+++ FlickrApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlickrApp
+//
+//  Created by Nikita Rodin on 11/12/14.
+//  Copyright (c) 2014 Artezio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlickrApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlickrApp
+//
+//  Created by Nikita Rodin on 11/12/14.
+//  Copyright (c) 2014 Artezio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
